21cc89b03ed897dc1876fc64e97f9a15adb5d43f
Cyril Ajieh <>
2019-02-07 03:24:37 -0500

    Implemented basic auth

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -6,17 +7,25 @@ import { Component, OnInit } from '@angular/core';
   div[fxLayout] {margin-top: 32px;}
 `],
   template: `
-  <div fxLayout="column" fxLayoutAlign="center center">
-  <span class="mat-display-2">Hello, Omonite!</span>
-  <button mat-raised-button color="primary" routerLink="/manager">Login as Manager</button>
-</div>
+  <div *ngIf="displayLogin">
+      <app-login></app-login>
+    </div>
+    <div *ngIf="!displayLogin">
+      <span class="mat-display-3">You get a lemon, you get a lemon, you get a lemon...</span>
+    </div>
   `
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  private _displayLogin = true;
+  constructor(private authService: AuthService) {}
 
   ngOnInit() {
+    this.authService.authStatus.subscribe(
+      authStatus => (this._displayLogin = !authStatus.isAuthenticated)
+    );
   }
 
+  get displayLogin() {
+    return this._displayLogin;
+  }
 }
